07e2f0c17825d4292f54c7cca884d88cf673766f
OMID837 <you@example.com>
2024-07-03 23:07:24 +0330

    new

--- account/views.py
+++ account/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class register(APIView):
-    def get(request):
-        pass
+    def get(self, request):
+        return Response('ok')
