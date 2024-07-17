4756c4433736bd86b45bc626d65a2c0dd20d11cf
OMID837 <you@example.com>
2024-07-17 11:50:53 +0330

    add all and add gitignorefile

--- DRFSHOPE/settings.py
+++ DRFSHOPE/settings.py
@@ -134,12 +134,6 @@ EMAIL_USE_TLS = env['EMAIL_USE_TLS']
 EMAIL_HOST_USER = env['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = env['EMAIL_HOST_PASSWORD']
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_USE_TLS = True
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'mrnobodyyy117@gmail.com'
-# EMAIL_HOST_PASSWORD = 'iufmgeiefnngxiwj'
-# EMAIL_PORT = 587
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
